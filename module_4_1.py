d7185d2c6fe6a067dcc7843391299feb1d5061cb
MARKOV4A <razvalyaevma@gmail.com>
2024-11-10 13:07:30 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as ntr
+from true_math import divide as tr
+
+fake = ntr(69, 3)
+fake_1 = ntr(3, 0)
+true = tr(49,7)
+true_1 = tr(15, 0)
+print(fake)
+print(fake_1)
+print(true)
+print(true_1)
